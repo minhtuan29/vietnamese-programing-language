e0c49d3d31b76ffa7d6fce30b186214d6429b4e6
minhtuan29 <86332370+minhtuan29@users.noreply.github.com>
2022-01-27 12:25:25 +0700

    them chuc nang tu dong them kytu close

--- main.js
+++ main.js
@@ -2,9 +2,28 @@ let runButton = document.getElementById("run_button");
 let userInputScript = document.getElementById("user_input");
 let output = document.getElementById("output");
 
+const closeChars = new Map([
+    ['{', '}'],
+    ['[', ']'],
+    ['(', ')']
+  ]);
+      
+  
+  userInputScript.addEventListener('input', function (e) {
+  
+      const pos = e.target.selectionStart;
+      const val = [...e.target.value];
+      
+      const char = val.slice(pos-1, pos)[0];
+      const closeChar = closeChars.get(char);
+      
+      if (closeChar) {
+        val.splice(pos, 0, closeChar);
+        e.target.value = val.join('');
+        e.target.selectionEnd = pos;
+      }
+  });
 
-// data = userInputScript.value;
-// output.innertext = ...
 
 let vietNamProgLangToJavaScriptToDictionary = {
     "cho " : "let ",
@@ -21,21 +40,17 @@ let vietNamProgLangToJavaScriptToDictionary = {
 
 
 
-
 let vietNamProgLanVocs = Object.keys(vietNamProgLangToJavaScriptToDictionary);
-
 
 
 
 runButton.addEventListener("click", ()=>{
     let currentResult = [];
     let script = userInputScript.value;
-  
 
     vietNamProgLanVocs.forEach(key =>
          script = script.replace(new RegExp(key, 'g'), vietNamProgLangToJavaScriptToDictionary[key])
     );
-
 
     eval(script);
     output.innerText = "";
