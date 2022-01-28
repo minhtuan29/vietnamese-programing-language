38e7d284f37a6c217135a9a37d7118afa6ef48b0
Tuan Hoang <86332370+minhtuan29@users.noreply.github.com>
2022-01-28 23:35:47 +0700

    Update intro.js

--- intro.js
+++ intro.js
@@ -97,4 +97,29 @@ console.log("xin chao, day la ngon ngu lap trinh bang tieng viet")
 console.log("xin chao, day la ngon ngu lap trinh bang tieng viet")
 console.log("xin chao, day la ngon ngu lap trinh bang tieng viet")
 console.log("xin chao, day la ngon ngu lap trinh bang tieng viet")
+    "cho " : "let ",
+    "viết" : "currentResult.push",
+    "nếu" : "if",
+    "không_thì" : "else",
+    "không_" : "!",
+    "đồng_thời" : "&&",
+    "hoặc" : "||",
+    "==" : "===",
+    "cho_hàm" : "function",
+    "ra" : "return",
+    "chạy_khi" : "while",
+    "tiếp_tục" : "continue",
+    "dừng_lại" :"break",
+    "với_mỗi" : "for",
+    "của_tập" : "of",
+    "các_pt_sẽ" : "forEach",
+    "lọc_với_đk" : "filter",
+    "tồn_tại" : "includes",
+    "tồn_tại_pt_thõa_đk" : "some",
+    "đều_thõa_mãn_đk" : "every",
+    "chuyển_đổi" : "map",
+    "lấy_về_các_pt_cuối" : "flat",
+    "tìm_pt_thõa_đk" : "find",
+    "sắp_xếp_với_đk" : "sort",
+    "tự_đảo_ngược" : "reverse"
 
